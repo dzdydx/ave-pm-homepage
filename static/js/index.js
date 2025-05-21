6bcb5495ab7d51e927dfd8ff634d1dc1683c3964
dzdydx <sirly.liu@qq.com>
2025-05-21 19:56:03 +0800

    finish basic page

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,39 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-
 $(document).ready(function() {
     // Check for click events on the navbar burger icon
 
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
+        slidesToScroll: 1,
+        slidesToShow: 3,
+        loop: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 8000,
+        pagination: false,
+        navigation: true,
+        navigationKeys: true,
+        breakpoints: [
+            {
+                changePoint: 1344, // 3 * 448px
+                slidesToShow: 3,
+                slidesToScroll: 1
+            },
+            {
+                changePoint: 896, // 2 * 448px
+                slidesToShow: 2,
+                slidesToScroll: 1
+            },
+            {
+                changePoint: 448, // 1 * 448px
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        ]
     }
 
-		// Initialize all div with carousel class
+    // Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
-	
+    
     bulmaSlider.attach();
-
 })
